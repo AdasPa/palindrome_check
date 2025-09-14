ac51d8c73eb5dc826ac9b6b7b7e6ab90faabcf9a
Adam Pacek <61830085+AdasPa@users.noreply.github.com>
2025-09-14 10:04:11 +0200

    Create palinfinder.s
    print_string function works correctly
    output messages work correctly

---
+++ palinfinder.s
@@ -0,0 +1,86 @@
+// palinfinder.s, provided with Lab1 in TDT4258 autumn 2025
+.global _start
+
+
+// Please keep the _start method and the input strings name ("input") as
+// specified below
+// For the rest, you are free to add and remove functions as you like,
+// just make sure your code is clear, concise and well documented.
+
+_start:
+	bl is_palindrome
+	mov r0, #65
+	bl is_not_palindrome
+	b _exit
+
+	
+check_input:
+	// You could use this symbol to check for your input length
+	// you can assume that your input string is at least 2 characters 
+	// long and ends with a null byte
+	
+	
+check_palindrome:
+	// Here you could check whether input is a palindrom or not
+	
+	
+is_palindrome:
+	push {lr}
+	
+	// Switch on only the 5 rightmost LEDs
+	// Write 'Palindrom detected' to UART
+	ldr r0, =detected_output
+	bl print_string
+	
+	pop {lr}
+	bx lr
+	
+is_not_palindrome:
+	push {lr}
+	// Switch on only the 5 leftmost LEDs
+	
+	// Write 'Not a palindrom' to UART
+	ldr r0, =not_detected_output
+	bl print_string
+	
+	pop {lr}
+	bx lr
+	
+print_string: // address of string in r0
+	push {r4, lr}
+	mov r4, r0
+	print_loop:
+	ldrb r0, [r4], #1
+	cmp r0, #0          // sprawdź, czy to null (koniec stringa)
+	beq string_printed  // jeśli tak -> koniec
+	bl PUT_JTAG         // wypisz znak
+	b print_loop        // pętla dalej
+
+	string_printed:
+	pop {r4, lr}
+	bx lr
+
+PUT_JTAG: // input param char in R0, assumes call by BL
+	LDR R1, =0xFF201000 // JTAG UART base address, assigned to R1
+	LDR R2, [R1, #4] // read the JTAG UART control register into R2
+	LDR R3, =0xFFFF0000 // mask, top 16 bits of control register holds write-space
+	ANDS R2, R2, R3 // Logical AND, R2 becomes zero if no space available, sets status register
+	BEQ PUT_JTAG // if no space, try again (Busy-waiting loop)
+	STR R0, [R1] // send the character by writing to UART data register
+	BX LR  // return from function using the Link Register LR
+	
+	
+_exit:
+	// Branch here for exit
+	b .
+	
+.data
+.align
+	// This is the input you are supposed to check for a palindrom
+	// You can modify the string during development, however you
+	// are not allowed to change the name 'input'!
+	test: .asciz "Test123"
+	input: .asciz "Grav ned den varg"
+	detected_output: .asciz "Palindrome detected\n"
+	not_detected_output: .asciz "Not a palindrome\n"
+.end
